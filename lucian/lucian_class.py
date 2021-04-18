86afe3a294bf67b80f9642e2eed137c90f94afef
Ugly_Potato <71345965+hyoseo837@users.noreply.github.com>
2021-04-18 09:40:36 +0200

    visual update

--- lucian/lucian_class.py
+++ lucian/lucian_class.py
@@ -16,7 +16,7 @@ class lucian(champion):
         if self.cool[1] <= 0:
             self.cool[1] = 6
             return bullet(self.name, self.ad*0.1, 150,\
-                 (self.posx,self.posy), self.direction, 100, 1,["", 2.5, True],number=1) 
+                 (self.posx,self.posy), self.direction, 200, 1,["", 2.5, True],number=1) 
 
     def skill2(self):
         if self.cool[2] <= 0:
